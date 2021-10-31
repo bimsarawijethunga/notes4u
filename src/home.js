af2a1d43e43c3fcf27b32651ccf9796a22c1170e
Bimsara Wijethunga <85950261+bimsarawijethunga@users.noreply.github.com>
2021-10-31 10:35:43 +0530

    Reported bugs fixed

--- src/home.js
+++ src/home.js
@@ -6,6 +6,8 @@ import Notes from './components/Notes';
 import Button from 'react-bootstrap/Button';
 import { getDatabase, ref, push, set, onValue} from "firebase/database";
 import { render } from '@testing-library/react';
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
 
 const home =()=> {
@@ -20,6 +22,7 @@ const home =()=> {
     }
 
     const createNote =(obj)=>  {
+        if(obj.title!='' || obj.note!=''){
         let x = Math.random();
         const auth = getAuth();
         const user = auth.currentUser;
@@ -32,7 +35,35 @@ const home =()=> {
             note: obj.note,
             uid: uid,
             kid: x,
-        });
+        });}
+        else{
+            confirmAlert({
+                title: 'Are You Sure?',
+                message: 'You are saving an Empty Note',
+                buttons: [
+                  {
+                    label: 'Yes',
+                    onClick: () => {let x = Math.random();
+                        const auth = getAuth();
+                        const user = auth.currentUser;
+                        const uid = user.uid;
+                        const db = getDatabase();
+                        const postListRef = ref(db, 'notes');
+                        const newPostRef = push(postListRef);
+                        set(newPostRef, {
+                            title: obj.title,
+                            note: obj.note,
+                            uid: uid,
+                            kid: x,
+                        });}
+                  },
+                  {
+                    label: 'No',
+                    onClick: () => {}
+                  }
+                ]
+              });
+        }
     }
         
         return(
@@ -46,13 +77,13 @@ const home =()=> {
                     }}>Notes4U</h1>
                     <Button style={{
                     fontSize: 20,
-                    position: "relative",
+                    position: "absolute",
                     top: 40,
-                    left: 40,
+                    right: 40,
                     marginTop: 20
-                    }} variant="danger" onClick = {signout}>Sign Out</Button></div><br/><br/><br/>       
-            <div className = 'screen'>
-                <text style={{marginRight:150}}>
+                    }} variant="danger" onClick = {signout}>Sign Out</Button></div>       
+            <div className = 'screen' style={{marginTop:100}}>
+                <text style={{marginRight:200}}>
                     <NotesForm createNote= {createNote}/>
                     </text>
                     <text>
